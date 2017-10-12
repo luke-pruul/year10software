cb1b5150eca6e8005c936bd3c31a7bedca158423
Luke Pruul <Luke.Pruul@education.nsw.gov.au>
2017-10-12 12:39:31 +1100

    made hello world and welcome message

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Wo1rld!");
+            Console.ReadLine();
         }
     }
 }
---
+++ welcomemessage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace welcomemessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+       
+        {
+
+            Console.Write("enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("welcome to my program " + name);
+
+
+            //wait at the end
+            Console.ReadKey();
+        }
+    }
+}
