6b1196eebff83223aa056804c6418b77b2386e94
Luke Pruul <Luke.Pruul@education.nsw.gov.au>
2017-11-07 14:58:56 +1100

    finished 8 ball

---
+++ magic 8 ball/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace magic_8_ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            playgame();
+        }
+
+        private static void playgame()
+        {
+            string[] answers = { "no", "yes" , "maybe" , "possibly" , "likely" , "not likely" };
+            Console.Write("ask me a question (or type ' exit'): ");
+            string choice = Console.ReadLine();
+            Random rnd = new Random();
+            Console.WriteLine(answers[rnd.Next(0,4)]);
+            Console.ReadKey();
+            playgame();
+           
+
+
+     }
+    }
+}
